6343b85a6545d81c84cd558acdc7c73097a96663
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-02-15 17:50:20 +0100

    creacion proyecto

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import Cabecera from './components/Cabecera.vue'
+</script>
+
+<template>
+<body>
+
+<Cabecera>ji</Cabecera>
+
+
+</body>
+</template>
+
+<style >
+
+</style>
